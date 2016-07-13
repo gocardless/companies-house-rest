9d3b11e3457953a6d7c3001e0c91cda5a0f3b497
Bruce Collie <bruce@gocardless.com>
2016-07-13 16:33:00 +0100

    Clean up code autogenerated by bundle

--- lib/companies/house/ruby.rb
+++
@@ -1,9 +0,0 @@
-require "companies/house/ruby/version"
-
-module Companies
-  module House
-    module Ruby
-      # Your code goes here...
-    end
-  end
-end
--- lib/companies/house/ruby/version.rb
+++
@@ -1,7 +0,0 @@
-module Companies
-  module House
-    module Ruby
-      VERSION = "0.1.0"
-    end
-  end
-end
