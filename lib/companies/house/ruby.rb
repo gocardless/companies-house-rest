0d741b38c53efe516d0ed59b1676629ee041b80e
Bruce Collie <bruce@gocardless.com>
2016-07-13 16:30:58 +0100

    Create new gem with bundle

---
+++ lib/companies/house/ruby.rb
@@ -0,0 +1,9 @@
+require "companies/house/ruby/version"
+
+module Companies
+  module House
+    module Ruby
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/companies/house/ruby/version.rb
@@ -0,0 +1,7 @@
+module Companies
+  module House
+    module Ruby
+      VERSION = "0.1.0"
+    end
+  end
+end
