1884eb8b467600a98b73f8f3be15e930896f2a2b
baltoli <bsc28@cam.ac.uk>
2016-07-19 17:04:23 +0100

    Company number validation
    Implement company number validation, using
    http://www.informdirect.co.uk/company-records/company-registration-number-crn-what-is-it/
    as a reference as to what formats are valid.

--- lib/companies-house.rb
+++ lib/companies-house.rb
@@ -5,6 +5,7 @@ require 'companies_house/api_error'
 require 'companies_house/not_found_error'
 require 'companies_house/authentication_error'
 require 'companies_house/rate_limit_error'
+require 'companies_house/registration_number.rb'
 
 module CompaniesHouse
   # The module that contains all the classes implementing the API client
