7059fbd013e519f70cec3beb6679527e611e634b
Bruce Collie <bruce@gocardless.com>
2016-07-19 12:32:02 +0100

    Allow default constructors for errors
    This is useful in testing, where we may not have sane data to pass to a
    constructor.

--- lib/companies_house/not_found_error.rb
+++ lib/companies_house/not_found_error.rb
@@ -4,7 +4,7 @@ module CompaniesHouse
   # Specific error class for when a company cannot be found (for example, if the company
   # number given is invaid)
   class NotFoundError < APIError
-    def initialize(company_id, response)
+    def initialize(company_id = nil, response = nil)
       super("Company #{company_id} not found", response)
     end
   end
--- lib/companies_house/rate_limit_error.rb
+++ lib/companies_house/rate_limit_error.rb
@@ -3,7 +3,7 @@
 module CompaniesHouse
   # Specific error class for when an invalid API key is used to access the service
   class RateLimitError < APIError
-    def initialize(response)
+    def initialize(response = nil)
       super("Rate limit exceeded", response)
     end
   end
