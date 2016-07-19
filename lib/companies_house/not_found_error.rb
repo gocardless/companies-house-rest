a8820605016377b3bdb74310917d23265f13fd98
baltoli <bsc28@cam.ac.uk>
2016-07-19 17:12:57 +0100

    Add nil to message explicitly

--- lib/companies_house/not_found_error.rb
+++ lib/companies_house/not_found_error.rb
@@ -5,7 +5,7 @@ module CompaniesHouse
   # number given is invaid)
   class NotFoundError < APIError
     def initialize(company_id = nil, response = nil)
-      super("Company #{company_id} not found", response)
+      super("Company #{company_id || 'nil'} not found", response)
     end
   end
 end
